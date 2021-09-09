15bd6238b6bb164ad7ef263a9cd7fe244d2678ae
Awilum <sergey.romanenko@flextype.org>
2021-09-09 20:13:52 +0300

    feat(extensions): add new extensions

---
+++ src/twig/core/Extensions/ParsersTwigExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Plugin\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class ParsersTwigExtension extends AbstractExtension
+{
+    /**
+     * Callback for twig.
+     *
+     * @return array
+     */
+    public function getFunctions() : array
+    {
+        return [
+            new TwigFunction('parsers', 'parsers')
+        ];
+    }
+}
---
+++ src/twig/core/Extensions/SlugifyTwigExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Flextype (https://flextype.org)
+ * Founded by Sergey Romanenko and maintained by Flextype Community.
+ */
+
+namespace Flextype\Plugin\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class SlugifyTwigExtension extends AbstractExtension
+{
+    /**
+     * Callback for twig.
+     *
+     * @return array
+     */
+    public function getFunctions() : array
+    {
+        return [
+            new TwigFunction('slugify', 'slugify')
+        ];
+    }
+}
